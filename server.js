b86d5a65acb8556094e801bf43a74bbec8cd096d
hapiz91 <hapiz91@gmail.com>
2026-05-18 18:33:36 +0400

    Initial Post91 Accounts MVP

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Post91 Accounts running on port ${PORT}`);
+});
